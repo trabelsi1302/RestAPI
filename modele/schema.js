881db9501100e1ee4e813e28673c7db88ff790c9
trabelsi1302 <elyestrfr@yahoo.fr>
2023-08-17 20:54:14 +0100

    first commit

---
+++ modele/schema.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age: Number,
+});
+
+const User = mongoose.model('User', userSchema);
